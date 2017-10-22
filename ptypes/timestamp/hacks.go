a31ddd03b2251ccd95142e56e8be9a2695b250b8
Rick Moran <drhayt@gmail.com>
2017-10-22 18:00:38 -0400

    Remove circular references

--- ptypes/timestamp/hacks.go
+++ ptypes/timestamp/hacks.go
@@ -6,21 +6,63 @@ import (
 
 	"database/sql/driver"
 
-	"github.com/golang/protobuf/ptypes"
+	"fmt"
 )
 
+// Conform to the Scanner interface for database/sql
 func (m *Timestamp) Scan(value interface{}) error {
-	if myTime, ok := value.(time.Time); ok {
-		foo, err := ptypes.TimestampProto(myTime)
-		if err != nil {
-			return err
-		}
-		m = foo
-		return nil
+	t, ok := value.(time.Time)
+	if !ok {
+		return errors.New("incompatible type passed, expected time.Time")
 	}
-	return errors.New("incompatible type passed, expected time.Time")
+
+	seconds := t.Unix()
+	nanos := int32(t.Sub(time.Unix(seconds, 0)))
+	m.Seconds = seconds
+	m.Nanos = nanos
+
+	return m.validateTimestamp()
 }
 
+// Value satisfies the valuer interface for database/sql.  Copied from ptypes.
 func (m *Timestamp) Value() (driver.Value, error) {
-	return ptypes.Timestamp(m)
+	var t time.Time
+	if m == nil {
+		t = time.Unix(0, 0).UTC() // treat nil like the empty Timestamp
+	} else {
+		t = time.Unix(m.Seconds, int64(m.Nanos)).UTC()
+	}
+	return t, m.validateTimestamp()
+}
+
+const (
+	// Seconds field of the earliest valid Timestamp.
+	// This is time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC).Unix().
+	minValidSeconds = -62135596800
+	// Seconds field just after the latest valid Timestamp.
+	// This is time.Date(10000, 1, 1, 0, 0, 0, 0, time.UTC).Unix().
+	maxValidSeconds = 253402300800
+)
+
+// validateTimestamp determines whether a Timestamp is valid.
+// A valid timestamp represents a time in the range
+// [0001-01-01, 10000-01-01) and has a Nanos field
+// in the range [0, 1e9).
+//
+// If the Timestamp is valid, validateTimestamp returns nil.
+// Otherwise, it returns an error that describes
+// the problem.
+//
+// Every valid Timestamp can be represented by a time.Time, but the converse is not true.
+func (m *Timestamp) validateTimestamp() error {
+	if m.Seconds < minValidSeconds {
+		return fmt.Errorf("timestamp: %v before 0001-01-01", m)
+	}
+	if m.Seconds >= maxValidSeconds {
+		return fmt.Errorf("timestamp: %v after 10000-01-01", m)
+	}
+	if m.Nanos < 0 || m.Nanos >= 1e9 {
+		return fmt.Errorf("timestamp: %v: nanos not in range [0, 1e9)", m)
+	}
+	return nil
 }
